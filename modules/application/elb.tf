d0a37ee8bbd3dab8792ad5ff2aaf32170c9069c4
Dipika.Pawar <dipika.m.pawar@gmail.com>
2021-06-30 23:01:49 +0100

    elb.tf synax error

--- modules/application/elb.tf
+++ modules/application/elb.tf
@@ -6,7 +6,6 @@ resource "aws_elb" "elb" {
   name            = "elb"
   security_groups = "${var.private_sg}-${var.public_sg}"
   subnets         = "${var.public_subnet_b}-${var.public_subnet_c}"
-
   health_check {
     healthy_threshold   = 2
     unhealthy_threshold = 2
@@ -14,7 +13,6 @@ resource "aws_elb" "elb" {
     interval            = 30
     target              = "HTTP:80/"
   }
-
   listener {
     lb_port           = 80
     lb_protocol       = "http"
