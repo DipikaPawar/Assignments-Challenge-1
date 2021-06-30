f381fde98fb1bcf3e797328aa88b868f9da5cb78
Dipika.Pawar <dipika.m.pawar@gmail.com>
2021-07-01 00:16:43 +0100

    error

--- modules/application/autoscaling.tf
+++ modules/application/autoscaling.tf
@@ -3,18 +3,18 @@
 # ------------------------------------------------------------------------------
 
 resource "aws_vpc" "main_vpc" {
-  cidr_block           = "${var.vpc_cidr}"
+  cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags {
-    Name = "main-vpc"
+    Name = "{var.main_vpc}-vpc"
   }
 }
 resource "aws_internet_gateway" "igw" {
   vpc_id = "${aws_vpc.main_vpc.id}"
 
   tags {
-    Name = "igw"
+    Name = "${var.main_vpc}-igw"
   }
 }
