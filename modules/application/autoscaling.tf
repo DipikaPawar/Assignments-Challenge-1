62126c00ee25cd3160d09c254926bcdd870e9478
Dipika.Pawar <dipika.m.pawar@gmail.com>
2021-06-30 23:59:46 +0100

    error

--- modules/application/autoscaling.tf
+++ modules/application/autoscaling.tf
@@ -3,7 +3,7 @@
 # ------------------------------------------------------------------------------
 
 resource "aws_vpc" "main_vpc" {
-  cidr_block           = "${var.vpc_cidr}"
+  cidr_block           = ${var.vpc_cidr}
   enable_dns_support   = true
   enable_dns_hostnames = true
 
@@ -13,7 +13,7 @@ resource "aws_vpc" "main_vpc" {
 }
 
 resource "aws_internet_gateway" "igw" {
-  vpc_id = "${aws_vpc.main_vpc.id}"
+  vpc_id = ${aws_vpc.main_vpc.id}
 
   tags {
     Name = "igw"
