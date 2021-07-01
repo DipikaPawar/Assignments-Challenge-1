e6c04cc672f84ff192f6f8f3645ed5f46c321b22
DipikaPawar <43720925+DipikaPawar@users.noreply.github.com>
2021-07-02 00:40:12 +0100

    Update elb.tf

--- modules/application/elb.tf
+++ modules/application/elb.tf
@@ -4,7 +4,7 @@
 
 resource "aws_elb" "elb" {
   name            = "elb"
-  security_groups = "[${var.private_sg}", "${var.public_sg}"]
+  #security_groups = "[${var.private_sg}", "${var.public_sg}"]
   subnets         = "[${var.public_subnet_b}", "${var.public_subnet_c}]"
   health_check {
     healthy_threshold   = 2
