ce887e87b907c968eb1ebc64329ab33225aa91b3
DipikaPawar <43720925+DipikaPawar@users.noreply.github.com>
2021-07-02 00:01:54 +0100

    Update vpc.tf

--- modules/network/vpc.tf
+++ modules/network/vpc.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "main_vpc" {
   enable_dns_support   = true
   enable_dns_hostnames = true
 
-  tags {
+  tags = {
     Name = "main-vpc"
   }
 }
@@ -15,7 +15,7 @@ resource "aws_vpc" "main_vpc" {
 resource "aws_internet_gateway" "igw" {
   vpc_id = "${aws_vpc.main_vpc.id}"
 
-  tags {
+  tags = {
     Name = "igw"
   }
 }
