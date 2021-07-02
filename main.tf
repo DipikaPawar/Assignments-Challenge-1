2b201a21159ad83c04ab6475c843d49cb56a730c
DipikaPawar <43720925+DipikaPawar@users.noreply.github.com>
2021-07-02 02:11:54 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,8 +21,10 @@
 provider "aws" {
   region = var.aws_region
 }
+
 module "vpc_module" {
-  source     = "./modules/vpc"
+  source     = "../00_modules/01_vpc"
+
   vpc_cidr =  var.vpc_cidr
   aws_region = var.aws_region
   application_name = var.application_name
@@ -35,8 +37,12 @@ module "vpc_module" {
   subnet_tier_private = var.subnet_tier_private
   country = var.country
 }
+
+
+
 module "elb_module" {
-  source     = "./modules/alb"
+  source     = "../00_modules/03_alb"
+
   vpc_cidr =  var.vpc_cidr
   aws_region = var.aws_region
   application_name = var.application_name
@@ -48,11 +54,16 @@ module "elb_module" {
   subnet_azs_private = var.subnet_azs_private
   subnet_tier_private = var.subnet_tier_private
   country = var.country
+
   vpc_id = module.vpc_module.vpc_main
-  alb-e-subnet = module.vpc_module.subnet_public 
+  alb-e-subnet = module.vpc_module.subnet_public
+
+  
 }
+
 module "ec2_module" {
-  source     = "./modules/ec2"
+  source     = "../00_modules/02_ec2"
+
   vpc_cidr =  var.vpc_cidr
   aws_region = var.aws_region
   application_name = var.application_name
@@ -64,22 +75,26 @@ module "ec2_module" {
   subnet_azs_private = var.subnet_azs_private
   subnet_tier_private = var.subnet_tier_private
   country = var.country
-  #vpc_main = module.vpc_module.vpc_main
-  #ami_id = var.ami_id
-  #ec2_monitoring = var.ec2_monitoring
-  #instance_type = var.instance_type
-  #ec2_names = var.ec2_names
-  #key_name = var.key_name
-  #ec2_disable_api_termination = var.ec2_disable_api_termination
-  #subnet_main = [module.vpc_module.subnet_private[0], module.vpc_module.subnet_private[1], module.vpc_module.subnet_private[2]]
-  #alb_tg = module.elb_module.alb_tg
-  #policy_list = var.policy_list
-  #asg_desired_capacity = var.asg_desired_capacity
-  #asg_max_size = var.asg_max_size
-  #asg_min_size = var.asg_min_size
+
+  vpc_main = module.vpc_module.vpc_main
+  ami_id = var.ami_id
+  ec2_monitoring = var.ec2_monitoring
+  instance_type = var.instance_type
+  ec2_names = var.ec2_names
+  key_name = var.key_name
+  ec2_disable_api_termination = var.ec2_disable_api_termination
+  subnet_main = [module.vpc_module.subnet_private[0], module.vpc_module.subnet_private[1], module.vpc_module.subnet_private[2]]
+  alb_tg = module.elb_module.alb_tg
+  policy_list = var.policy_list
+
+  asg_desired_capacity = var.asg_desired_capacity
+  asg_max_size = var.asg_max_size
+  asg_min_size = var.asg_min_size
 }
+
 module "rds_module" {
-  source     = "./modules/rds"
+  source     = "../00_modules/04_rds"
+
   vpc_cidr =  var.vpc_cidr
   aws_region = var.aws_region
   application_name = var.application_name
@@ -91,6 +106,7 @@ module "rds_module" {
   subnet_azs_private = var.subnet_azs_private
   subnet_tier_private = var.subnet_tier_private
   country = var.country
+
   rds_password = var.rds_password
   vpc_id = module.vpc_module.vpc_main
   rds_db_name = var.rds_db_name
