0c249ce30e2ea16e39e21e7372b3db9a92957ede
DipikaPawar <43720925+DipikaPawar@users.noreply.github.com>
2021-07-02 01:15:02 +0100

    Update elb.tf

--- modules/application/elb.tf
+++ modules/application/elb.tf
@@ -1,22 +1,20 @@
-# ---------------------------------------------------------------------------------------------------------------------
-# ELB
-# ---------------------------------------------------------------------------------------------------------------------
-
-resource "aws_elb" "elb" {
-  name            = "elb"
-  #security_groups = "[${var.private_sg}", "${var.public_sg}"]
-  subnets         = "[${var.public_subnet_b}", "${var.public_subnet_c}]"
+### Creating ELB####################
+###########################
+resource "aws_elb" "example" {
+  name = "terraform-asg-example"
+  security_groups = ["${aws_security_group.elb.id}"]
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
   health_check {
-    healthy_threshold   = 2
+    healthy_threshold = 2
     unhealthy_threshold = 2
-    timeout             = 3
-    interval            = 30
-    target              = "HTTP:80/"
+    timeout = 3
+    interval = 30
+    target = "HTTP:8080/"
   }
   listener {
-    lb_port           = 80
-    lb_protocol       = "http"
-    instance_port     = 80
+    lb_port = 80
+    lb_protocol = "http"
+    instance_port = "8080"
     instance_protocol = "http"
   }
 }
