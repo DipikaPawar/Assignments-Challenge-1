414bd62ca462ef84e3d683f84a47d3c849717fa4
DipikaPawar <43720925+DipikaPawar@users.noreply.github.com>
2021-07-02 01:39:22 +0100

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,55 +1,121 @@
-# ------------------------------------------------------------------------------
-# CONFIGURING 3 TIER ARCHITECTURE
-# ------------------------------------------------------------------------------
+## local state file
+
+ terraform {
+   backend "local" {
+     path    = "terraform.tfstate"
+   }
+ }
+
+## state file on s3
+
+# terraform {
+#   backend "s3" {
+#     bucket = "sample-bucket-name"
+#     key    = "common-services/terraform.tfstate"
+#     region = "ap-southeast-1"
+#     encrypt = true
+# 
+#   }
+# }
 
 provider "aws" {
-  region = "ap-southeast-2"
+  region = var.aws_region
 }
 
-#terraform {
-#backend "s3" {}
-#}
+module "vpc_module" {
+  source     = "../00_modules/01_vpc"
 
-# ------------------------------------------------------------------------------
-# RUNNING MODULES
-# ------------------------------------------------------------------------------
-
-module "network" {
-  source                = "./modules/network"
-  vpc_cidr              = "${var.vpc_cidr}"
-  public_subnet_b_cidr  = "${var.public_subnet_b_cidr}"
-  public_subnet_c_cidr  = "${var.public_subnet_c_cidr}"
-  private_subnet_b_cidr = "${var.private_subnet_b_cidr}"
-  private_subnet_c_cidr = "${var.private_subnet_c_cidr}"
-  db_subnet_b_cidr      = "${var.db_subnet_b_cidr}"
-  db_subnet_c_cidr      = "${var.db_subnet_c_cidr}"
-}
-
-module "application" {
-  source           = "./modules/application"
-  vpc_cidr         = "${var.vpc_cidr}"
+  vpc_cidr =  var.vpc_cidr
   aws_region = var.aws_region
   application_name = var.application_name
   environment = var.environment
-  public_subnet_b  = "${module.network.public_subnet_b}"
-  public_subnet_c  = "${module.network.public_subnet_c}"
-  private_subnet_b = "${module.network.private_subnet_b}"
-  private_subnet_c = "${module.network.private_subnet_c}"
-  public_sg        = "${module.network.public_sg}"
-  private_sg       = "${module.network.private_sg}"
+  subnet_cidr_private = var.subnet_cidr_private
+  subnet_azs_public = var.subnet_azs_public
+  subnet_tier_public = var.subnet_tier_public
+  subnet_cidr_public = var.subnet_cidr_public
+  subnet_azs_private = var.subnet_azs_private
+  subnet_tier_private = var.subnet_tier_private
   country = var.country
-  alb-e-subnet = module.vpc_module.subnet_public
 }
 
-module "database" {
-  source              = "./modules/database"
-  db_subnet_b         = "${module.network.db_subnet_b}"
-  db_subnet_c         = "${module.network.db_subnet_c}"
-  db_security_group   = "${module.network.private_sg}"
-  username            = "${var.username}"
-  password            = "${var.password}"
-  instance_class      = "${var.instance_class}"
-  multi_az            = "${var.multi_az}"
-  allocated_storage   = "${var.allocated_storage}"
-  skip_final_snapshot = "${var.skip_final_snapshot}"
+
+
+module "elb_module" {
+  source     = "../00_modules/03_alb"
+
+  vpc_cidr =  var.vpc_cidr
+  aws_region = var.aws_region
+  application_name = var.application_name
+  environment = var.environment
+  subnet_cidr_private = var.subnet_cidr_private
+  subnet_azs_public = var.subnet_azs_public
+  subnet_tier_public = var.subnet_tier_public
+  subnet_cidr_public = var.subnet_cidr_public
+  subnet_azs_private = var.subnet_azs_private
+  subnet_tier_private = var.subnet_tier_private
+  country = var.country
+
+  vpc_id = module.vpc_module.vpc_main
+  alb-e-subnet = module.vpc_module.subnet_public
+
+  
+}
+
+module "ec2_module" {
+  source     = "../00_modules/02_ec2"
+
+  vpc_cidr =  var.vpc_cidr
+  aws_region = var.aws_region
+  application_name = var.application_name
+  environment = var.environment
+  subnet_cidr_private = var.subnet_cidr_private
+  subnet_azs_public = var.subnet_azs_public
+  subnet_tier_public = var.subnet_tier_public
+  subnet_cidr_public = var.subnet_cidr_public
+  subnet_azs_private = var.subnet_azs_private
+  subnet_tier_private = var.subnet_tier_private
+  country = var.country
+
+  vpc_main = module.vpc_module.vpc_main
+  ami_id = var.ami_id
+  ec2_monitoring = var.ec2_monitoring
+  instance_type = var.instance_type
+  ec2_names = var.ec2_names
+  key_name = var.key_name
+  ec2_disable_api_termination = var.ec2_disable_api_termination
+  subnet_main = [module.vpc_module.subnet_private[0], module.vpc_module.subnet_private[1], module.vpc_module.subnet_private[2]]
+  alb_tg = module.elb_module.alb_tg
+  policy_list = var.policy_list
+
+  asg_desired_capacity = var.asg_desired_capacity
+  asg_max_size = var.asg_max_size
+  asg_min_size = var.asg_min_size
+}
+
+module "rds_module" {
+  source     = "../00_modules/04_rds"
+
+  vpc_cidr =  var.vpc_cidr
+  aws_region = var.aws_region
+  application_name = var.application_name
+  environment = var.environment
+  subnet_cidr_private = var.subnet_cidr_private
+  subnet_azs_public = var.subnet_azs_public
+  subnet_tier_public = var.subnet_tier_public
+  subnet_cidr_public = var.subnet_cidr_public
+  subnet_azs_private = var.subnet_azs_private
+  subnet_tier_private = var.subnet_tier_private
+  country = var.country
+
+  rds_password = var.rds_password
+  vpc_id = module.vpc_module.vpc_main
+  rds_db_name = var.rds_db_name
+  rds_instance_class = var.rds_instance_class
+  rds_cluster_instance_identifier = var.rds_cluster_instance_identifier
+  rds_cluster_identifier = var.rds_cluster_identifier
+  rds_username = var.rds_username
+  rds_engine = var.rds_engine
+  hostname = var.hostname
+  rds_engine_version = var.rds_engine_version
+  rds_subnets = [module.vpc_module.subnet_private[3], module.vpc_module.subnet_private[4], module.vpc_module.subnet_private[5]]
 }
